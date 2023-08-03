d5661a00aec539066bbe078e514f1103fa5d67e6
davidgaviriatatis <david.gaviria.tatis@gmail.com>
2023-08-03 12:45:17 -0500

    Solucion de conflicto GameManager
    Se arreglo el conflicto que se presento en el GameManager

--- Assets/Scripts/World/GameManager.cs
+++ Assets/Scripts/World/GameManager.cs
@@ -6,11 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public bool gameOver = false, winner = false, pause = false;
-<<<<<<< Updated upstream
-    public int gunAmmo = 20, health = 10, enemiesNumber = 20, enemiesSpawned = 0;
-=======
     public int gunAmmo = 10, health = 10, enemiesNumber = 30, enemiesSpawned = 0;
->>>>>>> Stashed changes
 
 
     private void Awake()
